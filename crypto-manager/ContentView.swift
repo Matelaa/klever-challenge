d235515f3d40122e2fe30e8668fa54f46a05aa39
Matelaa <jmatelaneto@gmail.com>
2023-11-30 17:21:12 -0300

    Initial Commit

---
+++ crypto-manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crypto-manager
+//
+//  Created by Matela on 30/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crypto-manager/crypto_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_managerApp.swift
+//  crypto-manager
+//
+//  Created by Matela on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct crypto_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

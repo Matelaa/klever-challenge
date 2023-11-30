69e4e4c7a2e31f82a14d226a2bad4637635f16e6
Matelaa <jmatelaneto@gmail.com>
2023-11-30 18:12:57 -0300

    creating a Service to get coins from api

--- crypto-manager/Service/CoinService.swift
+++ crypto-manager/Service/CoinService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class CoinService {
+    let baseURL: String = "https://api.coingecko.com/api/v3"
+    
+    func getCoins(completion: @escaping ([Coin]) -> Void) {
+        let url = URL(string: "\(baseURL)/coins/list")
+        
+        if let url = url {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    let coins = try? JSONDecoder().decode([Coin].self, from: data)
+                    if let coins = coins {
+                        completion(coins)
+                    }
+                } else {
+                    print("error: \(String(describing: error))")
+                }
+            }
+            task.resume()
+        }
+    }
+}

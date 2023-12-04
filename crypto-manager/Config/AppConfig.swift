c6fc7e73a4229505c743a4ec7b28f3ff6149523b
Matelaa <jmatelaneto@gmail.com>
2023-12-04 13:14:58 -0300

    setting configs of requests on privacy archive

--- crypto-manager/Config/AppConfig.swift
+++ crypto-manager/Config/AppConfig.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct AppConfig {
+    static var baseURL: String {
+        guard let path = Bundle.main.path(forResource: "ConfigInfo", ofType: "xcprivacy"),
+                let configDict = NSDictionary(contentsOfFile: path),
+                let baseURL = configDict["BaseURL"] as? String else {
+            fatalError("Arquivo de configuração ausente ou chave BaseURL não encontrada.")
+        }
+        return baseURL
+    }
+    
+    static var apiKey: String {
+        guard let path = Bundle.main.path(forResource: "ConfigInfo", ofType: "xcprivacy"),
+              let configDict = NSDictionary(contentsOfFile: path),
+              let apiKey = configDict["ApiKey"] as? String else {
+            fatalError("Arquivo de configuração ausente ou chave API não encontrada.")
+        }
+        return apiKey
+    }
+}

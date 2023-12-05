359954a1752b4a28a2b003aeaccefa77e6249b96
Matelaa <jmatelaneto@gmail.com>
2023-12-05 01:59:35 -0300

    creating the model to work withou realm

--- crypto-manager/Manager/Model/Manager.swift
+++ crypto-manager/Manager/Model/Manager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Manager: Identifiable {
+    var id: String
+    var coin: Coin?
+    var importance: Importance = .low
+    
+    enum Importance: Int {
+        case high = 2
+        case medium = 1
+        case low = 0
+    }
+}

5afccceb1ff6ef6f170e5c7ef4ef77a9f0df4266
Matelaa <jmatelaneto@gmail.com>
2023-12-05 02:00:14 -0300

    creating the methods of crud to manager coin

--- crypto-manager/Manager/ViewModel/ManagerViewModel.swift
+++ crypto-manager/Manager/ViewModel/ManagerViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class ManagerViewModel: ObservableObject {
+    @Published var managedCoins: [Manager] = []
+    @Published var alreadyManagedCoin: Bool = false
+    
+    func addCoinManagement(coin: Coin, importance: Importance) {
+        if RealmSingleton.shared.get().contains(where: { $0.id == coin.id }) {
+            self.alreadyManagedCoin = true
+            return
+        }
+        
+        let coinObject = CoinObject(coin: coin)
+        let manager = ManagerObject(coin: coinObject, importance: importance)
+        
+        try? RealmSingleton.shared.save(managerObject: manager)
+        
+        self.alreadyManagedCoin = false
+    }
+    
+    func getManagedCoins() {
+        self.managedCoins = RealmSingleton.shared.get()
+    }
+    
+    func deleteManagedCoin(managedCoin: Manager) {
+        self.managedCoins = RealmSingleton.shared.delete(managedCoin: managedCoin)
+    }
+    func updateManagedCoin() {
+        
+    }
+}

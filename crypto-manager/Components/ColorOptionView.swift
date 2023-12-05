c1a09a7fc3baaa1b522cc814f9ccafbac2ac0174
Matelaa <jmatelaneto@gmail.com>
2023-12-05 14:40:26 -0300

    create a screen to see a detail of ManagedCoin and component to ColorOptionView

---
+++ crypto-manager/Components/ColorOptionView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorOptionView.swift
+//  crypto-manager
+//
+//  Created by Matela on 05/12/23.
+//
+
+import SwiftUI
+
+struct ColorOptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorOptionView()
+}
---
+++ crypto-manager/Manager/View/ManagerCoinDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ManagerCoinDetailView.swift
+//  crypto-manager
+//
+//  Created by Matela on 05/12/23.
+//
+
+import SwiftUI
+
+struct ManagerCoinDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManagerCoinDetailView()
+}
